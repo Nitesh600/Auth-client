c719f66701ff7726f626bab6751901fc6dd51477
Nitesh Kumar <79629635+Nitesh600@users.noreply.github.com>
2025-01-24 14:50:23 +0530

    Update Login.jsx
    added link into login route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://auth-server-3.onrender.com/login",
+        "https://auth-server-4e5f.onrender.com/login",
         {
           ...inputValue,
         },
